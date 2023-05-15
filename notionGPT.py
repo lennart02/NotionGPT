9d5106db16ac775398560d60d0f9da9cc2fb1bd9
Lennart Pries <lp@tis-gmbh.de>
2023-05-15 10:25:48 +0200

    Schleife in der main funktion

--- notionGPT.py
+++ notionGPT.py
@@ -1,7 +1,9 @@
 from notion.client import NotionClient
 from notion.block import *
 import chatGPT
+import sys
 import json
+import time
 
 #New Way using Notion-py
 
@@ -13,8 +15,10 @@ def SearchPage(page):
             print(child.title.encode("utf-8"))
             try:
                 response = chatGPT.AskGPT3(child.title.replace("GPT!", ""))
-            except:
+            #except every error
+            except Exception as e:
                 response = "Chat GPT is not working right now."
+                print(e)
             if(len(response) < 600):
                 newchild = page.children.add_new(TextBlock, title="Answer: " + response)
                 newchild.move_to(child, "after")                
@@ -26,11 +30,19 @@ def SearchPage(page):
     
 if __name__ == "__main__":
     #searches in every path in the config.json file
+    args = sys.argv[1:]
+    num = 1
+    if len(args) != 0:
+        num = int(args[0])
     with open("config.json", "r") as f:
         data = json.load(f)
         client = NotionClient(token_v2=data["token"])
-        for i in data["pages"]:
-            page = client.get_block(i)
-            SearchPage(page)
+        for i in range(num):
+            for i in data["pages"]:
+                print("Searching " + i)
+                page = client.get_block(i)
+                SearchPage(page)
+            #sleeps for 30 seconds between each search except the first one
+            if num!= 1: time.sleep(30)
 
     
